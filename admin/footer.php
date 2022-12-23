1a6b4eb05c0d6ddbb81301187808d90d6b36d848
DestriPutriNatalia <desriputrinatalia@gmail.com>
2022-12-23 19:56:00 +0700

    first commit

---
+++ admin/footer.php
@@ -0,0 +1,11 @@
+<div class="container">   
+   <footer class="footer">
+      <div class="container">
+	  <div class="foot-margin">
+        <p><a>Copyright � 2022 by DClean  All Rightsreserved</a></p>
+      </div>
+      </div>
+    </footer>
+</div>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,17 @@
+    <footer class="footer">
+      <div class="container">
+	  <div class="foot-margin">
+          <p><a>Copyright � 2022 by DClean . All Rightsreserved</a></p>
+      </div>
+      </div>
+    </footer>
+	
+		<script type="text/javascript">
+			$(function() {
+				$('#da-thumbs > li').hoverdir();
+			});
+		</script>
+<?php include('tooltip.php'); ?>
+
+</body>
+</html>
